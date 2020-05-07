07dfe3f8cf6872d143af294abb14fb05503b20cb
JWSMartel <JWSMartel@gmail.com>
2020-05-07 17:23:10 -0400

    Added Factions

--- Android/app/src/main/java/com/example/galaxygenerator/MainActivity.java
+++ Android/app/src/main/java/com/example/galaxygenerator/MainActivity.java
@@ -59,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
         private static final String POP = "\nPopulation: ";
         private static final String GOVTYPE = "\nGovernment Type: ";
         private static final String LAW = "\nLaw Level: ";
-        private static final String HYPHEN = "-";
         private static final String TECHLV = "\nTech Level: ";
 
         private static final String TEMP = "\nTemperature: ";
@@ -302,7 +301,7 @@ public class MainActivity extends AppCompatActivity {
             Log.d(TAG, "Atmo Roll: "+planetoid.AtmoRoll);
             if(planetoid.AtmoRoll<2){
                 planetoid.Description += "Temperatures swing wildly from roasting during the day " +
-                        "to freezing at night.";
+                        "to freezing at night. ";
             }else if(planetoid.AtmoRoll == 2||planetoid.AtmoRoll == 3){
                 dieRoll -= 2;
             }else if(planetoid.AtmoRoll == 4||planetoid.AtmoRoll == 5 || planetoid.AtmoRoll == 14) {
@@ -467,26 +466,26 @@ public class MainActivity extends AppCompatActivity {
 
         private void createPlanetGov(Planetoid planetoid){
             Log.d(TAG, "createPlanetGov Called!");
-            planetoid.Description += "\n";
             if(planetoid.PopRoll == 0){
                 planetoid.Govt = "None";
                 planetoid.Description += "No government structure. In many cases, family bonds" +
                         "predominate. Examples include a Family, Clan, or total Anarchy. Little" +
                         " could be considered illegal here. ";
             }else{
-                GovRoll = rollDie(5,-5) + PopRoll;
-                if(GovRoll <= 0){
+                planetoid.GovRoll = rollDie(5,-5) + planetoid.PopRoll;
+                Log.d(TAG, "GovRoll: "+ planetoid.GovRoll);
+                if(planetoid.GovRoll <= 0){
                     planetoid.Govt = "None";
                     planetoid.Description += "No government structure. In many cases, family bonds" +
                             "predominate. Examples include a Family, Clan, or total Anarchy. Little" +
                             " could be considered illegal here. ";
                 }else{
-                    switch (GovRoll){
+                    switch (planetoid.GovRoll){
                         case 1:
                             planetoid.Govt = "Company/Corporation";
                             planetoid.Description += "Ruling functions are assumed by a company " +
                                     "managerial elite, and most citizenry are company employees " +
-                                    "or dependants. Examples include a corporate outpust, asteroid " +
+                                    "or dependants. Examples include a corporate outpost, asteroid " +
                                     "mine, or a feudal domain. Likely would not appreciate weapons, " +
                                     "drugs, or Travellers in their space. ";
                             break;
@@ -527,7 +526,7 @@ public class MainActivity extends AppCompatActivity {
                             break;
                         case 7:
                             planetoid.Govt = "Balkanisation";
-                            planetoid.Description += "No central authority exists; rival governments" +
+                            planetoid.Description += "No central authority exists rival governments" +
                                     " complete for control. Law Level refers to the government " +
                                     "nearest the starport. Examples include multiple governments " +
                                     "involved in a civil war. They could have an aversion to anything. ";
@@ -555,10 +554,165 @@ public class MainActivity extends AppCompatActivity {
                                     "leader, messiah, or emperor. Only the dictator can decide what " +
                                     "is illegal here. ";
                             break;
+                        case 11:
+                            planetoid.Govt = "Non-charismatic Leader";
+                            planetoid.Description += "A previous charismatic dictator " +
+                                    "has been replaced by a leader through normal channels. Examples include" +
+                                    " military dictatorship or hereditary kingship. They do not appreciate " +
+                                    "weapons, technology, or computers from other lands. ";
+                            break;
+                        case 12:
+                            planetoid.Govt = "Charismatic Oligarchy";
+                            planetoid.Description += "Ruling functions are performed" +
+                                    " by a select group of members of an organisation or class which enjoys " +
+                                    "the overwhelming confidence of the citizenry. Examples include junta, " +
+                                    "or a revolutionary council. They tend not to appreciate weapons around them. ";
+                            break;
                         default:
-                            Log.e(TAG, "Something went wrong with the government roll");
+                            planetoid.Govt = "Religious Dictatorship";
+                            planetoid.Description += "Ruling functions are " +
+                                    "performed by a religious organisation without regard to the specific " +
+                                    "individual needs ot the citizenry. Examples include cults, transcendent" +
+                                    " philosophy, or psionic group minds. Who is to say what they do or do " +
+                                    "not want in their sectors. ";
+                            break;
+                    }
+
+                    int factionRoll = rollDie(3,1);
+                    if(planetoid.GovRoll == 0||planetoid.GovRoll == 7){
+                         factionRoll += 1;
+                    }else if(planetoid.GovRoll > 10){
+                        factionRoll -= 1;
+                    }
+
+                    if(factionRoll > 0){
+                        planetoid.Description += "\n\nThere are "+factionRoll+" other factions on the " +
+                                "planet as well. ";
+                        for(int f=1;f<=factionRoll;f++){
+                            planetoid.Description += "\n\nSplinter faction #"+f+": ";
+                            rollFactionGov(planetoid);
+                        }
                     }
                 }
+            }
+        }
+
+        private void rollFactionGov(Planetoid planetoid){
+            int factionGovRoll = rollDie(13,0);
+            switch (factionGovRoll){
+                case 1:
+                    planetoid.Description += "Company/Corporation: Ruling functions are assumed by a company " +
+                            "managerial elite, and most citizenry are company employees " +
+                            "or dependants. Examples include a corporate outpost, asteroid " +
+                            "mine, or a feudal domain. Likely would not appreciate weapons, " +
+                            "drugs, or Travellers in their space. ";
+                    break;
+                case 2:
+                    planetoid.Description += "Participating Democracy: " +
+                            "Ruling functions are reached by the advise " +
+                            "and consent of the citizenry directly. Examples include a " +
+                            "collective, tribal council, or comm-linked consensus. Such " +
+                            "civilizations tend to be weary of drugs. ";
+                    break;
+                case 3:
+                    planetoid.Description += "Self-perpetuating Oligarchy: " +
+                            "Ruling functions are preformed by a restricted" +
+                            " minority, with little to no input from the mass of the " +
+                            "citizenry. Exmaples include a plutocracy or a hereditary ruling" +
+                            " caste. They do no tend to like weapons, technology, or Travellers. ";
+                    break;
+                case 4:
+                    planetoid.Description += "Representative Democracy: " +
+                            "Ruling functions are preformed by elected " +
+                            "representatives. Examples include a republic or democracy. " +
+                            "Likely would not appreciate drugs, weapons, or psionics.";
+                    break;
+                case 5:
+                    planetoid.Description += "Feudal Technocracy: " +
+                            "Ruling functions are preformed by specific " +
+                            "individuals for persons who agree to be ruled by them. " +
+                            "Relationships are based on the performance of technical " +
+                            "activities which are mutually beneficial.They do not tend to " +
+                            "like outside weapons, technology, or computers. ";
+                    break;
+                case 6:
+                    planetoid.Description += "Captive Government: " +
+                            "Ruling functions are performed by an imposed " +
+                            "leadership answerable to an outside group. Examples include a " +
+                            "colony or conquered area. Contraband here may include weapons, " +
+                            "technology, or Travellers. ";
+                    break;
+                case 7:
+                    planetoid.Description += "Balkanisation: No central authority exists rival governments" +
+                            " complete for control. Law Level refers to the government " +
+                            "nearest the starport. Examples include multiple governments " +
+                            "involved in a civil war. They could have an aversion to anything. ";
+                    break;
+                case 8:
+                    planetoid.Description += "Civil Service Bureaucracy: " +
+                            "Ruling functions are performed by government " +
+                            "agencies employing individuals selected for their expertise. " +
+                            "Examples include a technocracy or communism. They likely look " +
+                            "down on drugs and weapons. ";
+                    break;
+                case 9:
+                    planetoid.Description += "Impersonal Bureaucracy: " +
+                            "Ruling functions are performed by agencies " +
+                            "which have become insulated from the governed citizens. Examples " +
+                            "include entrenched castes of bureaucrats or a decaying empire. " +
+                            "They do like technology, weapons, drugs, Travellers, or " +
+                            "psionics typically. ";
+                    break;
+                case 10:
+                    planetoid.Description += "Charismatic Dictator : " +
+                            "Ruling functions are performed by agencies " +
+                            "directed by a single leader who enjoys the overwhelming " +
+                            "confidence of the citizens. Examples include a revolutionary " +
+                            "leader, messiah, or emperor. Only the dictator can decide what " +
+                            "is illegal here. ";
+                    break;
+                case 11:
+                    planetoid.Description += "Non-charismatic Leader : A previous charismatic dictator" +
+                            " has been replaced by a leader through normal channels. Examples include" +
+                            " military dictatorship or hereditary kingship. They do not appreciate " +
+                            "weapons, technology, or computers from other lands. ";
+                    break;
+                case 12:
+                    planetoid.Description += "Charismatic oligarchy : Ruling functions are performed" +
+                            " by a select group of members of an organisation or class which enjoys " +
+                            "the overwhelming confidence of the citizenry. Examples include junta, " +
+                            "or a revolutionary council. They tend not to appreciate weapons around them. ";
+                    break;
+                default:
+                    planetoid.Govt = "Religious Dictatorship";
+                    planetoid.Description += "Ruling functions are " +
+                            "performed by a religious organisation without regard to the specific " +
+                            "individual needs ot the citizenry. Examples include cults, transcendent" +
+                            " philosophy, or psionic group minds. Who is to say what they do or do " +
+                            "not want in their sectors. ";
+                    break;
+            }
+            if(factionGovRoll == planetoid.GovRoll){
+                planetoid.Description += "This is a splinter faction within the ruling government. ";
+            }
+            int factionStrengthRoll = rollDie(12,1);
+            planetoid.Description += "\n\nFaction strength: "+factionStrengthRoll+" - ";
+            if(factionStrengthRoll < 4){
+                planetoid.Description += "This faction is an obscure group. Few have heard of them " +
+                        "and they have no public support. ";
+            }else if(factionStrengthRoll < 6){
+                planetoid.Description += "This is a fringe group. There are few supporters throughout" +
+                        " the populace. ";
+            }else if(factionStrengthRoll < 8){
+                planetoid.Description += "This is a minor group with some supporters among the populace. ";
+            }else if(factionStrengthRoll < 10){
+                planetoid.Description += "This is a notable group here with a few significant and " +
+                        "well known members among its members, openly or otherwise. ";
+            }else if(factionStrengthRoll < 12){
+                planetoid.Description += "This faction is significant and could rival the primary government. ";
+            }else{
+                planetoid.Description += "This is an overwhelmingly popular faction, certainly with " +
+                        "more, if less than official, power than the primary government. ";
             }
         }
 
